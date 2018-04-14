53baa8b6731f28f1a6ad2c09574cdbeece14a735
Joan Marco <joanmarco96@gmail.com>
2018-04-14 18:56:15 +0200

    Started creating text comparisons

--- python/speak.py
+++ python/speak.py
@@ -7,3 +7,14 @@ def speak_out_loud(text=''):
     tts.save("tmp.mp3")
     os.system("mpg321 tmp.mp3")
     os.system("rm tmp.mp3")
+
+
+def compare(text=''):
+    if
+
+def answer(raw_input=''):
+   input = raw_input.split()
+
+
+
+
