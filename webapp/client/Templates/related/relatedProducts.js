2c8d67c34f3214c2a27f9d2fe0fb820e0fe19633
Jose Sentis <jose.sentis1987@gmail.com>
2018-04-15 01:28:47 +0200

    Image mirror loading after and fading in content

--- webapp/client/Templates/related/relatedProducts.js
+++ webapp/client/Templates/related/relatedProducts.js
@@ -1,36 +1,4 @@
 import { relatedProd } from './related.json';
-import Swiper from 'swiper/dist/js/swiper.js';
-//
-// const sliderOpts = {
-//   autoplay: {
-//     delay: 4000,
-//     disableOnInteraction: true
-//   },
-//   effect: 'slide',
-//   freeModeMomentum: true,
-//   loop: true,
-//   slidesPerView: 'auto',
-//   spaceBetween: 5,
-//   speed: 500,
-//   navigation: {
-//     nextEl: '.js-slider-next',
-//     prevEl: '.js-slider-prev'
-//   },
-//   keyboard: {
-//     enabled: true
-//   },
-//   a11y: {
-//     prevSlideMessage: 'Previous slide',
-//     nextSlideMessage: 'Next slide',
-//     firstSlideMessage: 'This is the first slide',
-//     lastSlideMessage: 'This is the last slide',
-//     paginationBulletMessage: 'Go to slide {{index}}'
-//   },
-// };
-
-Template.relatedProducts.rendered = function () {
-    // const gallery = new Swiper('.js-slider', sliderOpts);
-};
 
 Template.relatedProducts.helpers({
     products: function () {
