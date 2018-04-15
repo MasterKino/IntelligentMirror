3d135b41d72338c19762a040df73be9e7c5904d3
joanm <joanmarco96@gmail.com>
2018-04-15 02:43:58 +0200

    Divided server in two parts

--- python/main_logic.py
+++ python/main_logic.py
@@ -1,27 +1,11 @@
 import json
-from flask import Flask, Response, request
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from camera import VideoCamera
 from services import answer, confirmation, speak_out_loud
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/foo": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        if frame is not None:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-@app.route('/video_feed')
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/speak', methods=['GET', 'POST'])
